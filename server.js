23b0b4c720855cb340d3ed58233f5d9da1764955
mitul2589 <mitul.gandhi89@gmail.com>
2017-07-04 21:08:17 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -25,22 +25,61 @@ app.get('/api/products/', function (req, res) {
     var parr = [];
     mongoose.connect(url, function(err, db) {
         if(err) {
-            console.log("Error");
+           // console.log("Error");
         } else {
-            console.log("Yes, Connected...!!!!");
+           // console.log("Yes, Connected...!!!!");
         }
 
         Product.count((err, productsCount) => {
             if(err) {
-                console.log(err);
+                //console.log(err);
             } else {
-                console.log("productsCount" + productsCount);
+                //console.log("productsCount" + productsCount);
             }
 
             Product.find({}, (err, products) => {
                 res.json(products);      
             });
         });
+    });
+});
+
+app.post('/api/products/', function (request, res) {
+    var parr = [];
+
+    console.log("Request Paramerts" + request.query.param['name']);
+    console.log("Request Paramerts --->>" + request.param['name']);
+    console.log("Request Paramerts --->>" + request.params['name']);
+    mongoose.connect(url, function(err, db) {
+        if(err) {
+            console.log("Error");
+        } else {
+            console.log("Yes, Connected...!!!!");
+        }
+
+        var newproduct = new Product({
+            
+        "productId": 7,
+        "productName": "camera",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2016",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    
+        });
+        console.log("newproduct --->>>" + newproduct);
+
+        newproduct.save((err) => {
+            if(err) {
+                console.log("Error");
+            } else {
+                console.log("Saved Successfully...");
+            }
+        })
+
+        
     });
 });
 
